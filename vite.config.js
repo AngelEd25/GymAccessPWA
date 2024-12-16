29a7d5ab2412b3101d2404a19fa949c81a08ec2d
Yashiuwu <yashi.xuwux@gmail.com>
2024-12-16 13:30:14 -0600

    vite modified

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Asegura que las rutas relativas estén correctamente configuradas
-  build: {
-    outDir: 'dist', // Directorio donde se construirá la aplicación
-    assetsDir: '',  // Esto asegurará que los archivos como el service-worker.js se sirvan correctamente
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto', // Esto inyectará el Service Worker automáticamente
+      manifest: {
+        name: 'Tu App PWA',
+        short_name: 'App',
+        description: 'Una aplicación web progresiva',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'path/to/icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        // Aquí puedes personalizar más la configuración de Workbox si lo deseas
+      },
+    }),
+  ],
 });
