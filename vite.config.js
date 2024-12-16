5b7cce13a6bf3510263579446f224901a265a5d7
Yashiuwu <yashi.xuwux@gmail.com>
2024-12-16 13:10:57 -0600

    viteconfig modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Asegura que las rutas relativas estén correctamente configuradas
+  build: {
+    outDir: 'dist', // Directorio donde se construirá la aplicación
+    assetsDir: '',  // Esto asegurará que los archivos como el service-worker.js se sirvan correctamente
+  },
+});
