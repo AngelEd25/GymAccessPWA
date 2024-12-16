c7d585c1c2d59775d5aaf6c8d64ce16dad109eab
Yashiuwu <yashi.xuwux@gmail.com>
2024-12-16 12:11:25 -0600

    main.jsx  modified

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
+import * as serviceWorkerRegistration from "/serviceWorkerRegistration.js";
 
 // Select the root element from the DOM
 const rootElement = document.getElementById("root");
